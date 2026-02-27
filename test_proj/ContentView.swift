f50e95efe8649ccd88f1468f6479bdf812b2f50d
Andrew <andy.ulanov@gmail.com>
2026-02-27 21:56:17 +0400

    initial commit

---
+++ test_proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_proj
+//
+//  Created by andrew on 27.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_proj/test_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projApp.swift
+//  test_proj
+//
+//  Created by andrew on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct test_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
